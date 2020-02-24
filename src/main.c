ecbf908ee9be82aa6c9ad4e942ede01965b6d26e
Bryce Mecum <petridish@gmail.com>
2020-02-23 23:33:11 -0900

    Simplify still-broken example code

--- src/main.c
+++ src/main.c
@@ -1,12 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <netcdf.h>
+
 #include "emscripten.h"
 
 EMSCRIPTEN_KEEPALIVE
 int main() {
   int ncid;
-  int retval = nc_open("asdf.cdf", NC_NOWRITE, &ncid);
+  int retval;
+
+  // Just check symbols from netcdf.h work...
+  NC_INT;
+
+  // Try nc_create for now for testing/debuggin
+  retval = nc_create("simple_xy.nc", NC_CLOBBER, &ncid);
 
   return retval;
 }
